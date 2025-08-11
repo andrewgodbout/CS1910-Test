b4caaa5d731196b8479720a15a4391e5c5b5fea7
andrewgodbout <agodbout@upei.ca>
2025-08-11 13:52:13 -0300

    fix tests

--- HW/HW2/tests/q1.py
+++ HW/HW2/tests/q1.py
@@ -2,7 +2,7 @@ OK_FORMAT = True
 
 test = {   'name': 'q1',
     'points': 4,
-    'suites': [   {   'cases': [   {'code': ">>> q1_data[0].strip().replace(' ', '') == '(0,0)'\n['(0,0)', 'E', '0', 'initial_heading']\nTrue", 'hidden': False, 'locked': False},
+    'suites': [   {   'cases': [   {'code': ">>> q1_data[0].strip().replace(' ', '') == '(0,0)'\nTrue", 'hidden': False, 'locked': False},
                                    {'code': ">>> q1_data[1].strip()[0] == 'E'\nTrue", 'hidden': False, 'locked': False},
                                    {'code': '>>> int(q1_data[2].strip()) == 0\nTrue', 'hidden': False, 'locked': False},
                                    {'code': ">>> q1_data[3].strip() == 'initial_heading'\nTrue", 'hidden': False, 'locked': False}],
