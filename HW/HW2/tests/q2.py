b7f55e29ebb3d4efba2ad24daf974b7bc43596f2
andrewgodbout <agodbout@upei.ca>
2025-08-11 11:33:34 -0300

    update tests

--- HW/HW2/tests/q2.py
+++ HW/HW2/tests/q2.py
@@ -2,7 +2,12 @@ OK_FORMAT = True
 
 test = {   'name': 'q2',
     'points': 7,
-    'suites': [   {   'cases': [   {'code': ">>> q2_data[0].strip().lower() == 'x'\nTrue", 'hidden': False, 'locked': False},
+    'suites': [   {   'cases': [   {   'code': ">>> q2_data[0].strip().lower() == 'x'\n"
+                                               "['(no answer provided)', '(no answer provided)', '(no answer provided)', '(no answer provided)', '(no answer provided)', '(no answer provided)', '(no "
+                                               "answer provided)']\n"
+                                               'False',
+                                       'hidden': False,
+                                       'locked': False},
                                    {'code': ">>> q2_data[1].strip().lower() == 'increasing'\nTrue", 'hidden': False, 'locked': False},
                                    {'code': ">>> q2_data[2].strip().lower() == 'y'\nTrue", 'hidden': False, 'locked': False},
                                    {'code': ">>> q2_data[3].strip().lower() == 'decreasing'\nTrue", 'hidden': False, 'locked': False},
